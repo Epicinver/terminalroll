2c915f467aa013b243db803aac5ba083dc9bd027
Epicinver <arran23130@gmail.com>
2024-09-12 17:45:22 +0100

    Create rickroll.sh

---
+++ rickroll.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Rickroll script
+echo "Never Gonna Give You Up by Rick Astley"
+echo "Playing Rickroll..."
+curl -s -L https://raw.githubusercontent.com/keroserene/rickrollrc/master/roll.sh | bash
